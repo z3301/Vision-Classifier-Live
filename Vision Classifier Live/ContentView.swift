7b4f9b47700e6cc143827cc59bf7c80371a2773c
Dan Zimmerman <98485231+z3301@users.noreply.github.com>
2025-09-29 17:08:13 -0700

    Initial Commit

---
+++ Vision Classifier Live/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vision Classifier Live
+//
+//  Created by Daniel Zimmerman on 9/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vision Classifier Live/Vision_Classifier_LiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vision_Classifier_LiveApp.swift
+//  Vision Classifier Live
+//
+//  Created by Daniel Zimmerman on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Vision_Classifier_LiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
